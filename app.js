e7662504eaaa966d973e610eda9eab69d2cdb411
Samuel Baetz <samuelbaetz105@gmail.com>
2020-10-21 16:15:11 -0500

    saving cities

--- app.js
+++ app.js
@@ -39,13 +39,13 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
         
         $("#city1").text(city1)
 
-        if (city1 !== $("#search").val()) {
+        
             localStorage.setItem('city2', response.name)
             var city2 = localStorage.getItem('city2')
             $("#city2").text(city2)
             
-        }
-        console.log(city2)
+        
+        
         content.prepend(temp)
     });
 
