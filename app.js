4f51b0041abde305bad1d800e407eee8fcfbd78e
Samuel Baetz <samuelbaetz105@gmail.com>
2020-10-20 21:47:42 -0500

    stuff

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
     .then(function(response) {
 
         console.log(response)
-        var iconCode = response.weather[0].icon;
-        console.log(iconCode)
+        
+        console.log(response.weather[0].icon)
         $("#test").html(response.name)
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
         var temp = $("<h3>").text(tempF.toFixed() + " degrees")
