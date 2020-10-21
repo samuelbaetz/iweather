a82a6f40efe59148487a3625e289890034b84ae2
Samuel Baetz <samuelbaetz105@gmail.com>
2020-10-21 16:57:36 -0500

    rain

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 $( document ).ready(function() {
+
+
+    var makeItRain = function() {
+            
+            
+          
+        var increment = 0;
+        var drops = "";
+        var backDrops = "";
+      
+        while (increment < 100) {
+          //couple random numbers to use for various randomizations
+          //random number between 98 and 1
+          var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+          //random number between 5 and 2
+          var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+          //increment
+          increment += randoFiver;
+          //add in a new raindrop with various randomizations to certain CSS properties
+          drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+          backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+        }
+      
+        $('.rain').append(drops);
+        $('.rain').append(backDrops);
+      }
+     
     
     
 
@@ -47,6 +74,10 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
             var city2 = localStorage.getItem('city2')
             $("#city2").text(city2)
 
+        }
+
+        if(response.weather[0].main === "Rain") {
+            makeItRain()
         }
         
         content.prepend(temp)
@@ -297,37 +328,6 @@ $(window).on('load', function() {
 
 
 
-        var makeItRain = function() {
-            //clear out everything
-            
-          
-            var increment = 0;
-            var drops = "";
-            var backDrops = "";
-          
-            while (increment < 100) {
-              //couple random numbers to use for various randomizations
-              //random number between 98 and 1
-              var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
-              //random number between 5 and 2
-              var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
-              //increment
-              increment += randoFiver;
-              //add in a new raindrop with various randomizations to certain CSS properties
-              drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
-              backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
-            }
-          
-            $('.rain').append(drops);
-            $('.rain').append(backDrops);
-          }
-          
-          
-          
         
-          
-          
-          
-          makeItRain();
         
  });
