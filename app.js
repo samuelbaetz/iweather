de802700a7b8bb1774f9c9e76728bdc1f5b2f0a9
Samuel Baetz <samuelbaetz105@gmail.com>
2020-10-21 20:24:12 -0500

    radar

--- app.js
+++ app.js
@@ -237,20 +237,15 @@ $("#loc").on("click", function(){
 
     $("#radarbutton1").on("click", function(){
         $('#radarmodal1').modal('show')
-        // var APIKey = "166a433c57516f51dfab1f7edaed8413";
-        // var query = "https://tile.openweathermap.org/map/precipitation_new/0/0/0.png?appid=" + APIKey
-        // $.ajax({
-        //     url: query,
-        //     method: "GET"
-        //   })
-         
-        //     .then(function(response) {
-        
-        //         $("#radarimg").attr("src", query);
-                
-        
-        //     });
-
+        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic2FtdWVsYmFldHoiLCJhIjoiY2tnazU0aXJuMGpsMDJ0cWVqNnMyOHk1cyJ9.QIb_1jwmN4U4CkHqzIIAgg'
+}).addTo(mymap);
 
     })
     
