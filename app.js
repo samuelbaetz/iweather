9d219b2fe55b3a738ce695c342bd43fe97d97615
Samuel Baetz <samuelbaetz105@gmail.com>
2020-10-22 00:09:07 -0500

    radar

--- app.js
+++ app.js
@@ -60,6 +60,18 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
         $("#hum").text("Humidity: " + hum + "%")
         $("#windspeed").html("Wind Speed: " + wind + " MPH")
         var content = $("#test");
+        var lat = response.coord.lat
+        var lon = response.coord.lon
+
+        var mymap = L.map('mapid').setView([lat, lon], 13);
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoic2FtdWVsYmFldHoiLCJhIjoiY2tnazU0aXJuMGpsMDJ0cWVqNnMyOHk1cyJ9.QIb_1jwmN4U4CkHqzIIAgg'
+        }).addTo(mymap);
         
        
 
