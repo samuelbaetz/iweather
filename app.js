799ed7ae1901d4d1377bf1c5927d491f090f70a3
Samuel Baetz <samuelbaetz105@gmail.com>
2020-10-21 21:29:30 -0500

    radar

--- app.js
+++ app.js
@@ -329,7 +329,7 @@ $(window).on('load', function() {
                     $("#windspeed5").html("Wind Speed: " + response.list[31].wind.speed + " MPH")
                 });
 
-                var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+                var mymap = L.map('mapid').setView([lat, lon], 13);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
