1227e788afc79c7a195f413a89415dc870f7ef69
Samuel Baetz <samuelbaetz105@gmail.com>
2020-10-22 14:43:10 -0500

    radar

--- app.js
+++ app.js
@@ -352,8 +352,8 @@ $(window).on('load', function() {
     accessToken: 'pk.eyJ1Ijoic2FtdWVsYmFldHoiLCJhIjoiY2tnazU0aXJuMGpsMDJ0cWVqNnMyOHk1cyJ9.QIb_1jwmN4U4CkHqzIIAgg'
 }).addTo(mymap);
 var clouds = L.OWM.clouds({showLegend: false, opacity: 0.5, appId: '166a433c57516f51dfab1f7edaed8413'});
-var city = L.OWM.current({intervall: 15, lang: 'en'});
-var overlayMaps = { "Clouds": clouds, "Cities": city };
+var precipitation = L.OWM.precipitation({appId: '166a433c57516f51dfab1f7edaed8413'});
+var overlayMaps = { "Clouds": clouds, "Rain": precipitation };
 
 var layerControl = L.control.layers(overlayMaps).addTo(mymap);
 
