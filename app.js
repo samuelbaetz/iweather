5d8ca1c2c8c86477706a6b4762e44c229e3eacc0
Samuel Baetz <samuelbaetz105@gmail.com>
2020-11-04 18:40:17 -0500

    what

--- app.js
+++ app.js
@@ -88,8 +88,11 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
        var city = $('.city').append(`<button type="button" class="btn btn-success loc">${response.name}</button>`)
        
        localStorage.setItem('cities', city);
-       localStorage.getItem('cities')
+       var saved = localStorage.getItem('cities')
         
+       if (saved) {
+        $('.newcity') = saved;
+    }
         
 
         if(response.weather[0].main === "Rain") {
