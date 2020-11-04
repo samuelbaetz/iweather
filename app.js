1344f9e31110d6877f7559610f6ac086a84f87d1
Samuel Baetz <samuelbaetz105@gmail.com>
2020-11-04 15:37:54 -0500

    what

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
             localStorage.setItem('city1', response.name)
         var city1 = localStorage.getItem('city1')
         $('.newcity').append(`<li class="nav-item city1"></li>`)
-            .addclass("nav-item")
+        
             $('.city1').append(`<a class="nav-link" href="#">${city1}</a>`)
         
         }   
