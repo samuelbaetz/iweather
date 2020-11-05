0c2bdb2e837f628c9e2b428f62a9e14285f766fc
Samuel Baetz <samuelbaetz105@gmail.com>
2020-11-04 19:22:23 -0500

    what

--- app.js
+++ app.js
@@ -40,7 +40,85 @@ $( document ).ready(function() {
         }
     }
     
-    
+    $(".loc").on("click", function(event){
+        event.preventDefault()
+        for(i=0; i < cit.length; i++){
+        var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+          "q=" + cit[i] + "&appid=" + APIKey;
+        }
+          $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+         
+            .then(function(response) {
+        
+                console.log(response)
+                var iconCode = response.weather[0].icon;
+                var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+                console.log(iconUrl)
+                $(".icon").attr("src", iconUrl);
+                console.log(iconCode)
+                $(".test").html(response.name)
+                var hum = response.main.humidity
+                        var wind = response.wind.speed
+                var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+                var temp = $("<h3>").text(tempF.toFixed() + " degrees")
+                var content = $(".test");
+                $(".hum").text("Humidity: " + hum + "%")
+                $(".windspeed").html("Wind Speed: " + wind + " MPH")
+                var content = $(".test");
+                content.prepend(temp)
+            })
+
+            var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?" + "q=" + $("#search").val() + "&appid=" + APIKey
+            $.ajax({
+                url: queryURL2,
+                method: "GET"
+              })
+             
+                .then(function(response) {
+            
+                    console.log(response)
+                    console.log(response.list[0].weather[0].icon )
+                    $(".location").html(response.city.name)
+                    $(".icon1").attr("src", "https://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+                    $(".icon2").attr("src", "https://openweathermap.org/img/w/" + response.list[7].weather[0].icon + ".png");
+        
+                    $(".icon3").attr("src", "https://openweathermap.org/img/w/" + response.list[15].weather[0].icon + ".png");
+                    $(".icon4").attr("src", "https://openweathermap.org/img/w/" + response.list[23].weather[0].icon + ".png");
+                    $(".icon5").attr("src", "https://openweathermap.org/img/w/" + response.list[31].weather[0].icon + ".png");
+        
+                    $(".date1").html(response.list[0].dt_txt)
+                    $(".date2").html(response.list[7].dt_txt)
+                    $(".date3").html(response.list[15].dt_txt)
+                    $(".date4").html(response.list[23].dt_txt)
+                    $(".date5").html(response.list[31].dt_txt)
+        
+                    $(".hum1").text("Humidity: " + response.list[0].main.humidity + "%")
+                    $(".windspeed1").text("Wind Speed: " + response.list[0].wind.speed + " MPH")
+        
+                    $(".hum2").text("Humidity: " + response.list[7].main.humidity + "%")
+                    $(".windspeed2").text("Wind Speed: " + response.list[7].wind.speed + " MPH")
+        
+                    $(".hum3").text("Humidity: " + response.list[15].main.humidity + "%")
+                    $(".windspeed3").text("Wind Speed: " + response.list[15].wind.speed + " MPH")
+        
+                    $(".hum4").text("Humidity: " + response.list[23].main.humidity + "%")
+                    $(".windspeed4").text("Wind Speed: " + response.list[23].wind.speed + " MPH")
+        
+                    $(".hum5").text("Humidity: " + response.list[31].main.humidity + "%")
+                    $(".windspeed5").text("Wind Speed: " + response.list[31].wind.speed + " MPH")
+                });
+
+    })
+
+
+
+
+
 
 $("#click").on("click", function(event){
     
